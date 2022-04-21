f1a1e9ff3ef7dd46688a93ffd387a6a907eb26c9
bruna medeiros <brunamedeirosfelix@gmail.com>
2022-04-20 21:30:44 -0300

    foi adicionado função de atualizar deletar

--- routes/routs.js
+++ routes/routs.js
@@ -5,5 +5,8 @@ const TaskController = require("../controller/TaskController");
 //importando
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
+routes.get("/getById/:id/:method", TaskController.getById);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
+routes.get("/deleteOne/:id", TaskController.deleteOneTask);
 
 module.exports = routes;
